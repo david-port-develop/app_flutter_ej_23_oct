52e30e5fe7782541e72398c5f7615d39932cad90
david-port-develop <david.elisava@gmail.com>
2025-10-24 19:10:50 +0200

    Inicio

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_flutter_ej_23_oct
+
+App de flutter para desarrollar los ejercicios del día 23
---
+++ android/app/src/main/kotlin/com/example/app_flutter_ej_23_oct/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_flutter_ej_23_oct
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
